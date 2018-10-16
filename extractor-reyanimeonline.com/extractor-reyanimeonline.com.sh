18416278bc881a2704e8cda2d4bdcf87c89c0560
yonaikerlol <lawlietrs7@gmail.com>
2018-10-15 23:19:12 -0400

    Add menu to select between options [reyanimeonline]

--- extractor-reyanimeonline.com/extractor-reyanimeonline.com.sh
+++ extractor-reyanimeonline.com/extractor-reyanimeonline.com.sh
@@ -33,29 +33,77 @@ echo "${anime}:" > ".${1}.txt"
 echo "# ${anime}:" > ".${1}.min.txt"
 
 for (( f=1; f <= $2; f++ )); do
-	if [ ${f} -lt 10 ]; then
+	if [ "${f}" -lt 10 ]; then
 		echo -en "0${f}... ( ${underlined}http://reyanimeonline.com/ver/${1}-${f}${normal} )"
 	else
 		echo -en "${f}... ( ${underlined}http://reyanimeonline.com/ver/${1}-${f}${normal} )"
 	fi
 	req=$(curl -s "http://reyanimeonline.com/ver/${1}-${f}")
 
-	if echo "${req}" | grep -o "https://openload.co/embed/..........." &> /dev/null; then
-		link=$(echo "${req}" | grep -o "https://openload.co/embed/...........")
-	elif echo "${req}" | grep -o 'https://streamango.com/embed/.................' &> /dev/null; then
-		link=$(echo "${req}" | grep -o "https://streamango.com/embed/................")
-	elif echo "${req}" | grep -o 'ok.ru/videoembed/.............' &> /dev/null; then
-		link=$(echo "${req}" | grep -o 'ok.ru/videoembed/.............' | sed 's/^/https:\/\//g')
-	else
+	echo "${req}" | grep -o 'https://openload.co/embed/...........' &> /dev/null && openload=true
+	echo "${req}" | grep -o 'https://streamango.com/embed/.................' &> /dev/null && streamango=true
+	echo "${req}" | grep -o 'ok.ru/videoembed/.............' &> /dev/null && ok_ru=true
+	
+	if [ -z "${openload}" ] && [ -z "${streamango}" ] && [ -z "${ok_ru}" ]; then
 		if [ "${f}" -lt 10 ]; then
 			echo "0${f}:" >> ".${1}.txt"
 		else
 			echo "${f}:" >> ".${1}.txt"
 		fi
 		echo "#" > ".${1}.min.txt"
+		echo -e "\n\t${red}NOK!${normal}"
+		continue
+	fi
+
+	if [ "${openload}" = true ] && [ "${streamango}" = true ] && [ "${ok_ru}" = true ]; then
+		options="\n\t1. Openload\n\t2. Streamango\n\t3. OK.RU"
+		links=$(echo "${req}" | grep -o 'https://openload.co/embed/...........')
+		links="${links}\n$(echo "${req}" | grep -o 'https://streamango.com/embed/.................')"
+		links="${links}\n$(echo "${req}" | grep -o 'ok.ru/videoembed/.............' | sed 's/^/https:\/\//g')"
+	elif [ "${openload}" = true ] && [ "${streamango}" = true ]; then
+		options="\n\t1. Openload\n\t2. Streamango"
+		links=$(echo "${req}" | grep -o 'https://openload.co/embed/...........')
+		links="${links}\n$(echo "${req}" | grep -o 'https://streamango.com/embed/.................')"
+	elif [ "${openload}" = true ] && [ "${ok_ru}" = true ]; then
+		options="\n\t1. Openload\n\t2. OK.RU"
+		links=$(echo "${req}" | grep -o 'https://openload.co/embed/...........')
+		links="${links}\n$(echo "${req}" | grep -o 'ok.ru/videoembed/.............' | sed 's/^/https:\/\//g')"
+	elif [ "${streamango}" = true ] && [ "${ok_ru}" = true ]; then
+		options="\n\t1. Streamango\n\t2. OK.RU"
+		links=$(echo "${req}" | grep -o 'https://streamango.com/embed/.................')
+		links="${links}\n$(echo "${req}" | grep -o 'ok.ru/videoembed/.............' | sed 's/^/https:\/\//g')"
+	elif [ "${openload}" = true ]; then
+		options="\n\t1. Openload"
+		links=$(echo "${req}" | grep -o 'https://openload.co/embed/...........')
+	elif [ "${streamango}" = true ]; then
+		options="\n\t1. Streamango"
+		links=$(echo "${req}" | grep -o 'https://streamango.com/embed/.................')
+	elif [ "${ok_ru}" = true ]; then
+		options="\n\t1. OK.RU"
+		links=$(echo "${req}" | grep -o 'ok.ru/videoembed/.............' | sed 's/^/https:\/\//g')
+	fi
+
+	echo -e "\n\tOptions: ${options}"
+	echo -e "\t${red}WARNING:${normal} You have 10 seconds to answer, if you do not answer, option 1 will be chosen by default."
+	echo -en "\tSelect an option (a number): "
+
+	if ! read -t 10 -r optionSelected; then
+		optionSelected=1
+		echo ""
+	fi
+
+	if [ "${optionSelected}" -eq 0 ] || ! [[ "${optionSelected}" =~ ^[0-9]+$ ]]; then
+		if [ "${f}" -lt 10 ]; then
+			echo "0${f}:" >> ".${1}.txt"
+		else
+			echo "${f}:" >> ".${1}.txt"
+		fi
+		echo "#" >> ".${1}.min.txt"
 		echo -e "\t${red}NOK!${normal}"
 		continue
 	fi
+	
+	link=$(echo "${links}" | sed 's/\\n/\n/g' | sed -n -e "${optionSelected}p")
 	
 	if [ "${f}" -lt 10 ]; then
 		echo "0${f}: ${link}" >> ".${1}.txt"
